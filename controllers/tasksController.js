da184aa507715db4f37c1e3b703b9c81650e32c9
triagungtio07 <triagungtio07@gmail.com>
2021-01-16 10:22:42 +0700

    Inisiasi Controller API

--- controllers/tasksController.js
+++ controllers/tasksController.js
@@ -1,8 +1,8 @@
-class taskController{
-    get(req,res){}
-    post(req,res){}
-    put(req,res){}
-    delete(req,res){}
+class tasksController{
+    get(req,res){res.send("routing untuk read tasks")}
+    post(req,res){res.send("routing untuk create tasks")}
+    put(req,res){res.send("routing untuk update tasks")}
+    delete(req,res){res.send("routing untuk delete tasks")}
 }
 
-module.exports = Object.freeze(new taskController())
+module.exports = Object.freeze(new tasksController())
