59162ef0dd109f1dcf2b144159c5691edbcb80fb
triagungtio07 <triagungtio07@gmail.com>
2021-01-16 10:11:43 +0700

    Inisiasi Controller API

---
+++ controllers/tasksController.js
@@ -0,0 +1,8 @@
+class taskController{
+    get(req,res){}
+    post(req,res){}
+    put(req,res){}
+    delete(req,res){}
+}
+
+module.exports = Object.freeze(new taskController())
---
+++ controllers/usersController.js
@@ -0,0 +1,8 @@
+class UserController {
+    get(req,res){}
+    post(req,res){}
+    put(req,res){}
+    delete(req,res){}
+}
+
+module.exports = Object.freeze(new UserController())
